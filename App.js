845ce1eef84ad75c7514641f227ca0df8c121299
Mr.Dong <dong.job@foxmail.com>
2017-09-06 23:57:34 +0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Dyn_0212 on 2017/9/6.
+ */
+import React from 'react';
+
+class App extends React.Component {
+
+    render() {
+        return <div>Hello World!</div>
+    }
+}
+
+export default App
